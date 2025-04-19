ecbaf43d904ec1e7de782cbed39bee8c8f0312b1
Esha Patel <Patellesha11@gmail.com>
2025-04-19 21:50:39 +0300

    Done

--- src/Components/Dice.js
+++ src/Components/Dice.js
@@ -1,36 +1,38 @@
-import   useState  from "react";
+import useState from "react";
 import "./Dice.css";
 
-function Dice () {
-    const[diceNum, setDiceNum] = useState(1);
+function Dice() {
+  const [diceNum, setDiceNum] = useState(1);
 
-    const rollDice = () => {
-        const randomNum = Math.floor(Math.random() * 6) + 1;
-        setDiceNum(randomNum);
-      };
+  const rollDice = () => {
+    const randomNum = Math.floor(Math.random() * 6) + 1;
+    setDiceNum(randomNum);
+  };
 
-      const renderDots = () => {
-        const dotPattern = {
-            1: [5],
-            2: [1, 9],
-            3: [1, 5, 9],
-            4: [1, 3, 7, 9],
-            5: [1, 3, 5, 7, 9],
-            6: [1, 3, 4, 6, 7, 9],
-          };
+  const renderDots = () => {
+    const dotPattern = {
+      1: [5],
+      2: [1, 9],
+      3: [1, 5, 9],
+      4: [1, 3, 7, 9],
+      5: [1, 3, 5, 7, 9],
+      6: [1, 3, 4, 6, 7, 9],
+    };
 
-        return [...Array(9)].map((_, i) => (
-            <span
-              key={i}
-              className={`dot ${dotPatterns[diceNum].includes(i + 1) ? "active" : ""}`}
-            ></span>
-          ));
-        };
+    return [...Array(9)].map((_, i) => (
+      <span
+        key={i}
+        className={`dot ${
+          dotPatterns[diceNum].includes(i + 1) ? "active" : ""
+        }`}
+      ></span>
+    ));
+   };
 
-        return (
-            <div className="dice-cont">
-                  <h1>🎲 Dice Roller</h1>
-                  <div className="dice" onClick={rollDice}>
+  return (
+    <div className="dice-cont">
+      <h1>🎲 Dice Roller</h1>
+      <div className="dice" onClick={rollDice}>
         {renderDots()}
       </div>
       <button onClick={rollDice} className="roll-btn">
