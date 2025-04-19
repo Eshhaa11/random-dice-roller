142748c42c8509ec156a610e51daafd57b3ee71c
Esha Patel <Patellesha11@gmail.com>
2025-04-19 21:46:04 +0300

    added rolldice

--- src/Components/Dice.js
+++ src/Components/Dice.js
@@ -3,4 +3,9 @@ import "./Dice.css";
 
 function Dice () {
     const[diceNum, setDiceNum] = useState(1);
+
+    const rollDice = () => {
+        const randomNum = Math.floor(Math.random() * 6) + 1;
+        setDiceNumber(randomNum);
+      };
 }
